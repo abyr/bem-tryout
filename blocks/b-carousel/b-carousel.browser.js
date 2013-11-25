10be92c92ff3a502013136c9f87c88b20a0e3c55
Aleksandr Denisenko <abyrcheg@gmail.com>
2013-11-25 02:28:34 +0200

    block API

--- blocks/b-carousel/b-carousel.browser.js
+++ blocks/b-carousel/b-carousel.browser.js
@@ -32,6 +32,22 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
             this._autostart = this.domElem.data('autostart') || false;
 
             this.setMod('ready', true);
+        },
+
+        _togglePlay: function() {
+            console.log('_togglePlay');
+        },
+        _play: function() {
+            console.log('_play');
+        },
+        _stop: function() {
+            console.log('_stop');
+        },
+        _next: function() {
+            console.log('_next');
+        },
+        _prev: function() {
+            console.log('_prev');
         }
 
     });
