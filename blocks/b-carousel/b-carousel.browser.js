e32d5be9fad7e1460f0b140e4023f3124e7e1b1d
Aleksandr Denisenko <abyrcheg@gmail.com>
2013-11-25 02:22:31 +0200

    blank setup

--- blocks/b-carousel/b-carousel.browser.js
+++ blocks/b-carousel/b-carousel.browser.js
@@ -6,10 +6,23 @@ modules.define('i-bem__dom', ['jquery'], function(provide, $, DOM) {
         onSetMod: {
             'js': {
                 'inited': function() {
-                    console.log('hello');
+                    this._setup();
                 }
+            },
+            'ready': function() {
+                console.log('ready');
             }
+        },
+
+        _started: false,
+
+        _autostart: false,
+
+        _setup: function() {
+
+            this.setMod('ready', true);
         }
+
     });
 
     provide(DOM);
