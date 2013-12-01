a7a7c37cde40c74f68f203c5376a29b52b53e5a7
Aleksandr Denisenko <abyrcheg@gmail.com>
2013-12-01 21:56:41 +0200

    block content to be showed

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -18,6 +18,56 @@
             block: 'content',
             content: [
                 ' start ',
+                {
+                    block: 'b-carousel',
+                    mods: {
+                        autostart: true
+                    },
+                    content: [
+                        {
+                            title: 'Apple iPhone 4S 32Gb',
+                            image: 'http://mdata.yandex.net/i?path=b1004232748_img_id8368283111385023010.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b1004232748_img_id8368283111385023010.jpg'
+                        },
+                        {
+                            title: 'Samsung Galaxy Ace S5830',
+                            image: 'http://mdata.yandex.net/i?path=b0206005907_img_id5777488190397681906.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b0206005907_img_id5777488190397681906.jpg'
+                        },
+                        {
+                            title: 'HTC One S',
+                            image: 'http://mdata.yandex.net/i?path=b0229115752_img_id5362588736801424341.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b0229115752_img_id5362588736801424341.jpg'
+                        },
+                        {
+                            title: 'Sharp SH530U',
+                            image: 'http://mdata.yandex.net/i?path=b1119145405_img_id7474251838115767025.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b1119145405_img_id7474251838115767025.jpg'
+                        },
+                        {
+                            title: 'Samsung Galaxy S III 16Gb',
+                            image: 'http://mdata.yandex.net/i?path=b0503234231_img_id4954748565558111178.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b0503234231_img_id4954748565558111178.jpg'
+                        },
+                        {
+                            title: 'Samsung Galaxy Ace S5830',
+                            image: 'http://mdata.yandex.net/i?path=b0206005907_img_id5777488190397681906.jpg',
+                            url: 'http://mdata.yandex.net/i?path=b0206005907_img_id5777488190397681906.jpg'
+                        }
+                    ],
+                    controls: [
+                        {
+                            elem: 'button',
+                            type: 'prev',
+                            content: '<'
+                        },
+                        {
+                            elem: 'button',
+                            type: 'next',
+                            content: '>'
+                        }
+                    ]
+                },
                 ' end '
             ]
         },
